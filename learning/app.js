3c83e385a852c7d314c8964751676f85db41fcad
aneme1038 <thriftman25@gmail.com>
2020-02-05 21:42:25 -0500

    testing api request

--- learning/app.js
+++ learning/app.js
@@ -1,1 +1,13 @@
-
+$(() => {
+  const $btn = $('#btn1');
+  $($btn).on('click', (event) => {
+    console.log("this is working");
+    $.ajax({
+      url: "https://api.pushshift.io/reddit/search/comment/?q=science&subreddit=askscience",
+      type: "GET"
+    }).then((data) => {
+      console.log(data.results);
+      const results = data.results;
+    })
+  })
+})
