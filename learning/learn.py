385c8841538ae3851bb670bc572b8336a462bee3
aneme1038 <thriftman25@gmail.com>
2020-02-11 15:34:01 -0500

    added loops on dictionaries exercise

--- learning/learn.py
+++ learning/learn.py
@@ -83,3 +83,28 @@ inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold'] = inventory['gold'] + 50
+
+#supermarket exercise practicing dictionaries and loops
+prices = {
+  "banana" : 4,
+  "apple"  : 2,
+  "orange" : 1.5,
+  "pear"   : 3,
+}
+stock = {
+  "banana" : 6,
+  "apple"  : 0,
+  "orange" : 32,
+  "pear"   : 15,
+}
+
+for key in prices:
+  print key
+  print "price: %s" % prices[key]
+  print "stock: %s" % stock[key]
+print
+print
+total = 0
+for key in prices:
+  total += (prices[key] * stock[key])
+print total
