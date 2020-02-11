b111d7dc0328979c6da60cdde56ecbbc697df647
aneme1038 <thriftman25@gmail.com>
2020-02-10 22:01:52 -0500

    added new freeform project to learning

--- learning/learn.py
+++ learning/learn.py
@@ -24,3 +24,44 @@ elif option == 'T':
 else:
   print "You entered an invalid shape/entry!"
 print "Program is now exiting"
+
+"""
+This program should do the following:
+1. Roll a pair of dice.
+2. Add the values of the roll.
+3. Ask the user to guess a number.
+4. Compare the user's guess to the total value.
+5. Determine the winner (user or computer)
+"""
+
+from random import randint
+from time import sleep
+
+def get_user_guess():
+  guess = int(raw_input("Guess a number between 2 and 12"))
+  return guess
+def roll_dice(number_of_sides):
+  first_roll = randint(1, number_of_sides)
+  second_roll = randint(1, number_of_sides)
+  max_val = number_of_sides * 2
+  print "The maximum value of the dice is %d" % (max_val)
+  guess = get_user_guess()
+  if guess > max_val:
+    print "What you entered is invalid."
+  else:
+    print "Rolling..."
+    sleep(2)
+    print "The first rolled dice shows a %d" % (first_roll)
+    sleep(1)
+    print "The second rolled dice shows a %d" % (second_roll)
+    sleep(1)
+    total_roll = first_roll + second_roll
+    print total_roll
+    print "Result..."
+    sleep(1)
+    if guess == total_roll:
+      print "You won! You guessed the roll!"
+    else:
+      print "You lost! Game Over! Try Again!"
+
+roll_dice(6)
