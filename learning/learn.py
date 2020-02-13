3cbcdfe4e005a13c24d3b60f217e220035024628
aneme1038 <thriftman25@gmail.com>
2020-02-12 20:14:23 -0500

    starting python 3 learning

--- learning/learn.py
+++ learning/learn.py
@@ -163,3 +163,12 @@ def play_RPS():
   decide_winner(user_choice, computer_choice)
 #call the play_RPS function to start game
 play_RPS()
+
+##########
+#PYTHON 3
+#########
+def repeat_stuff(stuff, num_repeats = 10):
+  return stuff*num_repeats
+lyrics = repeat_stuff("Row ", 3) + "Your Boat. "
+song = repeat_stuff(lyrics)
+print(song)
