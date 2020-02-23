3cfeb70d83cf384db1661c5f05d87f64c80f8797
aneme1038 <thriftman25@gmail.com>
2020-02-22 19:56:13 -0500

    django project testing

---
+++ django-test/pollster/polls/urls.py
@@ -0,0 +1,15 @@
+#created this file to create the paths for linking templates
+#need this line to get Path
+from django.urls import path
+#import from ALL the VIEWS
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
+    #passing in the question_id from views for detail
+    path('<int:question_id>/', views.detail, name='detail'),
+    #passing question_id again for the results view
+    path('<int:question_id>/results/', views.results, name='results')
+
+]
