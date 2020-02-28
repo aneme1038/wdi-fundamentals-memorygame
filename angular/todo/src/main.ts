ab3667778c945cbf7d5a5fcab1ed3f8193c20ac9
aneme1038 <thriftman25@gmail.com>
2020-02-28 16:57:51 -0500

    completed a simplistic todo application in angular 9

--- angular/todo/src/main.ts
+++ angular/todo/src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//calling this method is for browser-based applications. If working on different platform, you will have to use a different bootstrap method specific to that platform. You should be able to find documentation regarding how to do handle this call for that platform. 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
